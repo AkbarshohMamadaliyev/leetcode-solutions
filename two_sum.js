e082a449fced0412db39f9dbdc2f2b83e55b0cba
AkbarshohMamadaliyev <akbarshokhmamadaliev@gmail.com>
2026-01-03 22:55:35 +0500

    completed two_sum

---
+++ two_sum.js
@@ -0,0 +1,27 @@
+/*
+ * Problem: Two Sum
+ * Link: https://leetcode.com/problems/two-sum
+ *
+ * Approach:
+ * - Inner For Loop
+ *
+ * Runtime: 50 ms
+ * Memory: 55.72 MB
+ */
+
+function twoSum(nums, target) {
+  let result = [];
+
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        result.push(i, j);
+      }
+    }
+  }
+
+  return result;
+}
+
+// Test run:
+console.log(twoSum([2, 7, 11, 15], 9));
